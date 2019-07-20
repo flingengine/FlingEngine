61c03e48fa3b2afdea9778c258ca7ba6951b84ef
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-07-19 23:31:33 -0400

    Add generate docs folder and move all html docs up a dir, so that they show up on git.io

---
+++ GenerateDocs.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+doxygen doxyfile
+cd docs/html
+mv * ..
---
+++ docs/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "bcdefgilmpqrstuvw~",
+  1: "eflqrstv",
+  2: "f",
+  3: "eflmprstuv",
+  4: "cdefgilmpqrstuw~",
+  5: "bcfgmpst",
+  6: "iu",
+  7: "cfgs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Macros"
+};
+
