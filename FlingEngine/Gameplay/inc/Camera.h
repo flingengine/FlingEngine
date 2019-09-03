2afabad6efd7e55cfecb9d3391ae87907756de77
artur <arturokuang@gmail.com>
2019-09-03 16:15:03 -0400

    Calculate view and prespecitve matrix

--- FlingEngine/Gameplay/inc/Camera.h
+++ FlingEngine/Gameplay/inc/Camera.h
@@ -16,14 +16,26 @@ namespace Fling
 
 		~Camera() {};
 
+		void Update();
+
 		const float& GetNearPlane() { return m_nearPlane; }
 		void SetNearPlane(const float& nearPlane) { m_nearPlane = nearPlane; }
 
 		const float& GetFarPlane() { return m_farPlane; }
 		void SetFarPlane(const float& farPlane) { m_farPlane = farPlane; }
 
+		const float& GetFieldOfView() { return m_fieldOfView; }
+		void SetFieldOfView(const float& fieldOfView) { m_fieldOfView = fieldOfView; }
+
+		const glm::vec3& GetPosition() { return m_position; }
+		const glm::vec3& GetRotation() { return m_rotation; }
+		const glm::vec3& GetVelocity() { return m_velocity; }
+
+		const glm::mat4& GetViewMatrix() {return m_viewMatrix; }
+		const glm::mat4& GetProjectionMatrix() { return m_ProjectionMatrix; }
 
 	private:
+		float height, width;
 		float m_nearPlane;
 		float m_farPlane;
 		float m_fieldOfView;
@@ -31,6 +43,8 @@ namespace Fling
 		glm::vec3 m_position;
 		glm::vec3 m_rotation;
 		glm::vec3 m_velocity;
+		glm::vec3 m_front;
+		glm::vec3 m_up;
 
 		glm::mat4 m_viewMatrix;
 		glm::mat4 m_projectionMatrix;
--- FlingEngine/Gameplay/src/Camera.cpp
+++ FlingEngine/Gameplay/src/Camera.cpp
@@ -1,4 +1,19 @@
-#include "pch.h"
 #include "Camera.h"
+
+namespace Fling
+{
+    void Camera::Update()
+    {
+        m_projectionMatrix = glm::prespective(
+            m_fieldOfView, 
+            width, 
+            height, 
+            m_nearPlane, 
+            m_farplane);
+
+        m_viewMatrix = glm::lookAt(m_position, m_position + m_front, m_up);
+    }
+}
+
 
 
