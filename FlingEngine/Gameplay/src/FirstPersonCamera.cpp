a79000588dd01095c3a3cba0e50d81dcc862a731
artur <arturokuang@gmail.com>
2019-09-05 21:53:30 -0400

    camera can now be controlled with keyboard

--- FlingEngine/Gameplay/src/FirstPersonCamera.cpp
+++ FlingEngine/Gameplay/src/FirstPersonCamera.cpp
@@ -13,9 +13,32 @@ namespace Fling
     }
 
 
-    void FirstPersonCamera::Update()
+    void FirstPersonCamera::Update(float dt)
     {
-        
+
+        glm::vec3 cameraFront;
+
+        cameraFront.x = -cos(glm::radians(m_rotation.x)) * sin(glm::radians(m_rotation.y));
+		cameraFront.y = sin(glm::radians(m_rotation.x));
+		cameraFront.z = cos(glm::radians(m_rotation.x)) * cos(glm::radians(m_rotation.y));
+		cameraFront = glm::normalize(cameraFront);
+
+        float moveSpeed = m_speed * dt;
+
+        if(Input::IsKeyDown(KeyNames::FL_KEY_W))
+				m_position += cameraFront * moveSpeed;
+
+        if(Input::IsKeyDown(KeyNames::FL_KEY_A))
+				m_position -= cameraFront * moveSpeed;
+
+        if(Input::IsKeyDown(KeyNames::FL_KEY_S))
+                m_position -= glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f))) * moveSpeed;
+
+        if(Input::IsKeyDown(KeyNames::FL_KEY_D))
+                m_position += glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f))) * moveSpeed;
+
+        UpdateViewMatrix();
+        UpdateProjectionMatrix();
     }
 
     void FirstPersonCamera::UpdateViewMatrix()
