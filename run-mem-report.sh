ea720d7311b3bd65e89eaad38490f52bc4428812
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-09-17 13:14:05 -0400

    Updaet memory reportnig script

--- run-mem-report.sh
+++ run-mem-report.sh
@@ -1,1 +1,1 @@
-valgrind --log-file="MEM_REPORT.txt" --leak-check=full --show-leak-kinds=all ./build/Sandbox/bin/Sandbox
+valgrind --log-file="MEM_REPORT.txt" --leak-check=yes --track-origins=yes ./build/Sandbox/bin/Sandbox
