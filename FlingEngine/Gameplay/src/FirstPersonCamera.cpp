40b8d06ce4af34f5d1521544b45ee6857b3c02ce
artur <arturokuang@gmail.com>
2019-09-27 18:37:05 -0400

    cleaned up code and added comments

--- FlingEngine/Gameplay/src/FirstPersonCamera.cpp
+++ FlingEngine/Gameplay/src/FirstPersonCamera.cpp
@@ -16,9 +16,7 @@ namespace Fling
 		m_fieldOfView = glm::radians(45.0f);
 		m_position = glm::vec3(0.0f, 0.0f, 5.f);
 		m_rotation = glm::vec3(0.0f, 0.0f, 0.0f);
-		m_worldUp = glm::vec3(0.0f, 0.0f, 1.0f);
-
-		m_yaw = 0.0f, m_pitch = 0.0f;
+		m_worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 		UpdateProjectionMatrix();
     }
@@ -113,16 +111,16 @@ namespace Fling
 		MousePos CurMousePos = Input::GetMousePos();
 		if (m_IsRotating)
 		{
-			float RotSpeed = dt * m_RotationSpeed * .001f;
+			float RotSpeed = dt * m_RotationSpeed;
 
 			float MouseDeltaX = CurMousePos.X - m_PrevMousePos.X;
 			float MouseDeltaY = CurMousePos.Y - m_PrevMousePos.Y;
 
 			// Invert x rotation because it otherwise it's south paw
-			m_yaw += RotSpeed * -MouseDeltaX;	
-			m_pitch += RotSpeed * MouseDeltaY;
+			m_rotation.x += RotSpeed * -MouseDeltaX;	
+			m_rotation.y += RotSpeed * MouseDeltaY;
 
-			m_pitch = glm::clamp(m_pitch, -MAX_PITCH, MAX_PITCH);
+			m_rotation.y = glm::clamp(m_rotation.y, -MAX_PITCH, MAX_PITCH);
 		}
 		
 		// Keep track of the mouse position
@@ -144,9 +142,9 @@ namespace Fling
 
 	void Fling::FirstPersonCamera::UpdateCameraVectors()
 	{
-		glm::mat4x4 rotation = glm::eulerAngleYX(m_yaw, m_pitch);
-		m_front = rotation * glm::vec4(0.f, 0.f, -1.f, 0.f);
-		m_up = rotation * glm::vec4(0.f, 1.f, 0.f, 0.f);
+		glm::mat4x4 rotation = glm::eulerAngleYX(m_rotation.x, m_rotation.y);
+		m_front = rotation * glm::vec4(m_worldFront, 0.f);
+		m_up = rotation * glm::vec4(m_worldUp, 0.f);
 		m_right = glm::cross(m_front, m_up);
 	}
 
