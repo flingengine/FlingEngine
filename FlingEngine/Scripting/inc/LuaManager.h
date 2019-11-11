28872a85935f38218db9550e1b9aab6ac2299607
Seth Leyens <sel6606@g.rit.edu>
2019-11-11 14:18:11 -0500

    -Changing the includes in LuaManager.h again

--- FlingEngine/Scripting/inc/LuaManager.h
+++ FlingEngine/Scripting/inc/LuaManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "pch.h"
+#include "Platform.h"
+#include "Singleton.hpp"
 #include "ScriptComponent.h"
 
 namespace Fling
--- FlingEngine/Scripting/inc/ScriptComponent.h
+++ FlingEngine/Scripting/inc/ScriptComponent.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "pch.h"
 
 namespace Fling
 {
