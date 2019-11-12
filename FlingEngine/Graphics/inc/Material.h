6d8063fe1a9115f6cbb89beb608e159e49437cf3
artur <arturokuang@gmail.com>
2019-11-12 18:47:06 -0500

    fixed spacing in material

--- FlingEngine/Graphics/inc/Material.h
+++ FlingEngine/Graphics/inc/Material.h
@@ -7,42 +7,42 @@
 
 namespace Fling
 {
-	/**
-	* @brief the properties of a PBR 
-	*/
-	struct PBRTextures
-	{
-		Image* m_AlbedoTexture		= nullptr;
-		Image* m_NormalTexture		= nullptr;
-		Image* m_RoughnessTexture	= nullptr;
-		Image* m_MetalTexture		= nullptr;
-	};
+    /**
+    * @brief the properties of a PBR 
+    */
+    struct PBRTextures
+    {
+        Image* m_AlbedoTexture        = nullptr;
+        Image* m_NormalTexture        = nullptr;
+        Image* m_RoughnessTexture    = nullptr;
+        Image* m_MetalTexture        = nullptr;
+    };
 
-	/**
-	* @brief	A material represents what properties should be given to a set
-	*			of shaders. This is referenced by the MeshRednerer and Renderer::DrawFrame
-	*/
+    /**
+    * @brief    A material represents what properties should be given to a set
+    *            of shaders. This is referenced by the MeshRednerer and Renderer::DrawFrame
+    */
     class Material : public JsonFile
     {
-		friend class Renderer;
-	public:
+        friend class Renderer;
+    public:
 
-		static std::shared_ptr<Fling::Material> Create(Guid t_ID);
+        static std::shared_ptr<Fling::Material> Create(Guid t_ID);
 
-		explicit Material(Guid t_ID);
+        explicit Material(Guid t_ID);
 
         const PBRTextures& GetTexture() const { return m_Textures; }
 
         const ShaderPrograms::ShaderProgramType GetShaderProgramType() { return m_ShaderProgram; }
-	private:
+    private:
 
-		void LoadMaterial();
+        void LoadMaterial();
 
-		// Textures that this material uses
-		PBRTextures m_Textures = {};
+        // Textures that this material uses
+        PBRTextures m_Textures = {};
         
         ShaderPrograms::ShaderProgramType m_ShaderProgram;
 
-		float m_Shininiess = 0.5f;        
+        float m_Shininiess = 0.5f;        
     };
 }   // namespace Fling
--- FlingEngine/Graphics/src/Material.cpp
+++ FlingEngine/Graphics/src/Material.cpp
@@ -3,22 +3,22 @@
 #include "ResourceManager.h"
 
 namespace Fling
-{	
-	std::shared_ptr<Fling::Material> Material::Create(Guid t_ID)
-	{
-		return ResourceManager::LoadResource<Fling::Material>(t_ID);
-	}
+{    
+    std::shared_ptr<Fling::Material> Material::Create(Guid t_ID)
+    {
+        return ResourceManager::LoadResource<Fling::Material>(t_ID);
+    }
 
-	Material::Material(Guid t_ID)
-		: JsonFile(t_ID)
-	{
-		LoadMaterial();
-	}
+    Material::Material(Guid t_ID)
+        : JsonFile(t_ID)
+    {
+        LoadMaterial();
+    }
 
-	void Material::LoadMaterial()
-	{
-		try
-		{
+    void Material::LoadMaterial()
+    {
+        try
+        {
             // Load Shaders -------------
             const std::string& ShaderProgram = m_JsonData["ShaderProgram"];
             if (ShaderProgram == "PBR" || ShaderProgram == "pbr")
@@ -34,27 +34,27 @@ namespace Fling
                 assert("Shader type not supported! ");
             }
 
-			// Load Textures -------------
-			// Albedo
-			const std::string& AlbedoPath = m_JsonData["albedo"];
-			m_Textures.m_AlbedoTexture = Image::Create(HS(AlbedoPath.c_str())).get();
+            // Load Textures -------------
+            // Albedo
+            const std::string& AlbedoPath = m_JsonData["albedo"];
+            m_Textures.m_AlbedoTexture = Image::Create(HS(AlbedoPath.c_str())).get();
 
-			// Normal
-			const std::string& NormalPath = m_JsonData["normal"];
-			m_Textures.m_NormalTexture = Image::Create(HS(NormalPath.c_str())).get();
+            // Normal
+            const std::string& NormalPath = m_JsonData["normal"];
+            m_Textures.m_NormalTexture = Image::Create(HS(NormalPath.c_str())).get();
 
-			// Metal
-			const std::string& MetalPath = m_JsonData["metal"];
-			m_Textures.m_MetalTexture = Image::Create(HS(MetalPath.c_str())).get();
+            // Metal
+            const std::string& MetalPath = m_JsonData["metal"];
+            m_Textures.m_MetalTexture = Image::Create(HS(MetalPath.c_str())).get();
 
-			// Rough
-			const std::string& RoughPath = m_JsonData["rough"];
-			m_Textures.m_RoughnessTexture = Image::Create(HS(RoughPath.c_str())).get();
-		}
-		catch (std::exception& e)
-		{
-			F_LOG_ERROR("Failed to load material file {} : {}", GetFilepathReleativeToAssets(), e.what());
-			FLING_BREAK();
-		}
-	}
+            // Rough
+            const std::string& RoughPath = m_JsonData["rough"];
+            m_Textures.m_RoughnessTexture = Image::Create(HS(RoughPath.c_str())).get();
+        }
+        catch (std::exception& e)
+        {
+            F_LOG_ERROR("Failed to load material file {} : {}", GetFilepathReleativeToAssets(), e.what());
+            FLING_BREAK();
+        }
+    }
 }   // namespace Fling
