64fc71e30ebea27fd9f8664ae119be87988462c7
artur <arturokuang@gmail.com>
2019-11-28 22:49:33 -0500

    added comments to stack allocator

--- FlingEngine/Utils/inc/StackAllocator.h
+++ FlingEngine/Utils/inc/StackAllocator.h
@@ -8,14 +8,38 @@
 
 namespace Fling
 {
+    /**
+     * @brief 
+     * 
+     * @see https://blog.molecular-matters.com/2012/08/27/memory-allocation-strategies-a-stack-like-lifo-allocator/
+     */
     class StackAllocator
     {
     public:
+        /**
+         * @brief Construct a new Stack Allocator object
+         * 
+         * @param t_Start  Start of the memory block to use for this stack allocator
+         * @param t_End    End of the memory block to use for this stack allocator 
+         */
         StackAllocator(void* t_Start, void* t_End);
         ~StackAllocator();
 
+        /**
+         * @brief 
+         * 
+         * @param t_Size        Size of the block of memory 
+         * @param t_Alignment   Alignment of the element (perfer sizes are a power of 2)
+         * @param t_Offset      Offset of the element
+         * @return void*        Obtain a chunk of memory of the size, alignment, and offset (asserts when we exceed preallocated size) 
+         */
         void* Allocate(size_t t_Size, size_t t_Alignment, size_t t_Offset);
 
+        /**
+         * @brief Returns a block of memory to the stack in a LIFO manner 
+         * 
+         * @param t_Ptr 
+         */
         void Free(void* t_Ptr);
 
     private:
