10f1c24f8bc2d981c14b87eed609519083f6fff5
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-12-09 09:29:44 -0500

    Remove the defintion that was undefined with templates

--- FlingEngine/Utils/inc/Memory.h
+++ FlingEngine/Utils/inc/Memory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "FlingTypes.h"
 
 namespace Fling
@@ -10,9 +11,10 @@ namespace Fling
     void AlignedFree(void* t_Data);
 
 #if FLING_LINUX
-    #define leading_zeroes(x) ((x) == 0 ? 32 : __builtin_clz(x))
-    #define trailing_zeroes(x) ((x) == 0 ? 32 : __builtin_ctz(x))
-    #define trailing_ones(x) __builtin_ctz(~(x))
+
+    #define leading_zeroes(x)       ((x) == 0 ? 32 : __builtin_clz(x))
+    #define trailing_zeroes(x)      ((x) == 0 ? 32 : __builtin_ctz(x))
+    #define trailing_ones(x)        __builtin_ctz(~(x))
 #elif FLING_WINDOWS
 #include <intrin.h>
 
@@ -37,22 +39,11 @@ namespace Fling
         }
     }
 
-#define leading_zeroes(x) Fling::Internal::clz(x)
-#define trailing_zeroes(x) Fling::Internal::ctz(x)
-#define trailing_ones(x) Fling::Internal::ctz(~(x))
+#define leading_zeroes(x)       Fling::Internal::clz(x)
+#define trailing_zeroes(x)      Fling::Internal::ctz(x)
+#define trailing_ones(x)        Fling::Internal::ctz(~(x))
 
 #endif
-
-    template<typename T>
-    inline void for_each_bit(uint32_t value, const T& func)
-    {
-        while (value)
-        {
-            UINT32 bit = trailing_zeroes(value);
-            func(bit);
-            value &= ~(1u << bit);
-        }
-    }
 
     //Shift the given address upwards if/as necessary to ensure it is aligned to
     //the given number of bytes 
