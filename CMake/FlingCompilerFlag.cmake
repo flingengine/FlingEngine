97a0ad930400b4f67331bf00b311f8ca5168c944
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2022-11-05 17:15:45 -0400

    Add a little cmake macro that will add a nice Cmake Option and set the name of a define to on or off

---
+++ CMake/FlingCompilerFlag.cmake
@@ -0,0 +1,23 @@
+# Just a small macro that will make a CMake project option, and 
+# then add a #define associated to that set to the value of 0 or 1
+# depending on if the option is on or not
+
+
+MACRO( FLING_Compiler_Flag FlagName DefineName Description DefaultValue )
+
+    OPTION( ${FlagName} ${Description} ${DefaultValue} ) 
+
+    # If the option is enabled, then define the value to 1
+    IF( ${FlagName} )
+        add_compile_definitions( ${DefineName}=1)
+        message( Status "${DefineName}=1" )
+    # otherwise, set it to false
+    else()
+        add_compile_definitions( ${DefineName}=0)
+        message( Status "${DefineName}=0" )  
+    endif()    
+
+    #target_compile_definitions(trie_io_test PRIVATE UNIT_TESTING=1 IO_TEST=1)
+
+
+ENDMACRO(FLING_Compiler_Flag)
