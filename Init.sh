b0b36e5c5f98cd48c51567eb0c8f372bcd3f2bde
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2025-04-13 09:53:38 -0400

    Update init.sh to include installing Catch2 libraries so that cmake doesnt fail when first syncing the project

--- Init.sh
+++ Init.sh
@@ -3,11 +3,15 @@ echo   =============================================================
 echo ================= Fling Engine initalize script =================
 echo   =============================================================
 
-# Create the build folders if they are not created already
-mkdir build -p
-
 # Make sure that we have all external libraries that we need
+echo Initalizing submodules...
 git submodule update --init --recursive
 
+# Install Catch2 so that CMake can find it
+echo Installing Catch2 testing library...
+cmake ./external/Catch2 -B ./external/Catch2/build
+sudo make install --directory=./external/Catch2/build -j$(nproc)
+
 # Run cmake!
+echo Running CMake 
 cmake . -B build
